d4089cd07fc2d582c6b2234cbb975fd16b679ddc
Alex Eagle <theherocraft@gmail.com>
2023-03-29 21:39:16 +0200

    Reuse the client

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,18 +1,9 @@
-import mongoose from 'mongoose'
-import mongooseOptions from '../options/mongoose'
-
+import MongooseClient from '../clients/MongooseClient'
 import User from './User'
 
 const getModels = async () => {
-  // https://mongoosejs.com/docs/guide.html#strictQuery
-  mongoose.set('strictQuery', false)
+  await MongooseClient.connect()
 
-  // Ensure connection is open so we can run migrations
-  if (mongoose.connection.readyState !== 1) {
-    await mongoose.connect(process.env.MIGRATE_MONGO_URI ?? 'mongodb://localhost:27017/express', mongooseOptions)
-  }
-
-  // Return models that will be used in migration methods
   return {
     User
   }
