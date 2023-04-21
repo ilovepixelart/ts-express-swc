bef2aeebbe2d33d1dc1679d3e748cade43c1c51d
Alex Eagle <theherocraft@gmail.com>
2023-04-22 00:24:28 +0200

    Dep

--- migrations/1679248134387-test.ts
+++ migrations/1679248134387-test.ts
@@ -3,22 +3,14 @@ import getModels from '../src/models'
 export async function up () {
   console.log('Migration - up')
   const { User } = await getModels()
+
   // Write migration here
-  const created = await User.create([
-    {
-      name: 'John',
-      role: 'user'
-    },
-    {
-      name: 'Alice',
-      role: 'user'
-    }
-  ])
+  await User.findOneAndUpdate({ name: 'John Doe' }, { name: 'John Doe', role: 'admin' }, { upsert: true, new: true })
 }
 
 export async function down () {
   console.log('Migration - down')
   const { User } = await getModels()
   // Write migration here
-  const deleted = await User.deleteMany({ role: 'user' }).exec()
+  await User.deleteMany({ role: 'admin' })
 }
